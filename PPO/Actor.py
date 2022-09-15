c1df246be9fb48f2c9ee82f5add2647111203afe
ma-zeller <49483996+ma-zeller@users.noreply.github.com>
2022-09-15 11:25:49 +0200

    Updates

--- PPO/Actor.py
+++ PPO/Actor.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense
-from tensorflow.keras.optimizers import Adam, RMSprop
 from tensorflow.keras import backend as K
 
 import numpy as np
@@ -9,41 +8,55 @@ import numpy as np
 class Actor_Model():
     def __init__(self, input_shape, action_space, lr, optimizer):
 
-        X_input = Input(input_shape)
+        # Format input
+        input_Act = Input(input_shape)
         self.action_space = action_space
 
-        X = Dense(512, activation="relu", kernel_initializer=tf.random_normal_initializer(stddev=0.01))(X_input)
-        X = Dense(256, activation="relu", kernel_initializer=tf.random_normal_initializer(stddev=0.01))(X)
-        X = Dense(64, activation="relu", kernel_initializer=tf.random_normal_initializer(stddev=0.01))(X)
-        output = Dense(self.action_space, activation="softmax")(X)
+        # Define Network structure
+        X_Act = Dense(512, activation="relu", kernel_initializer=tf.random_normal_initializer(stddev=0.01))(input_Act)
+        X_Act = Dense(256, activation="relu", kernel_initializer=tf.random_normal_initializer(stddev=0.01))(X_Act)
+        X_Act = Dense(64, activation="relu", kernel_initializer=tf.random_normal_initializer(stddev=0.01))(X_Act)
 
-        self.Actor = Model(inputs = X_input, outputs = output)
+        # Get final output
+        output_Act = Dense(self.action_space, activation="softmax")(X_Act)
+
+        # Create Actor
+        self.Actor = Model(inputs = input_Act, outputs = output_Act)
         self.Actor.compile(loss=self.ppo_loss, optimizer=optimizer(lr=lr))
 
+    # Define ProximalPoliyOptimization actor loss
     def ppo_loss(self, y_true, y_pred):
+
+        # Use GAEs
         advantages, prediction_picks, actions = y_true[:, :1], y_true[:, 1:1+self.action_space], y_true[:, 1+self.action_space:]
         LOSS_CLIPPING = 0.2
         ENTROPY_LOSS = 0.001
         
+        # Get probs
         prob = actions * y_pred
         old_prob = actions * prediction_picks
 
+        # Clip
         prob = K.clip(prob, 1e-10, 1.0)
         old_prob = K.clip(old_prob, 1e-10, 1.0)
 
+        # Calculate update ratio
         ratio = K.exp(K.log(prob) - K.log(old_prob))
-        
+
+        #Calculate actor loss PPO style
         p1 = ratio * advantages
         p2 = K.clip(ratio, min_value=1 - LOSS_CLIPPING, max_value=1 + LOSS_CLIPPING) * advantages
-
         actor_loss = -K.mean(K.minimum(p1, p2))
 
+        # Calculate entropy
         entropy = -(y_pred * K.log(y_pred + 1e-10))
         entropy = ENTROPY_LOSS * K.mean(entropy)
         
+        # Get final loss
         total_loss = actor_loss - entropy
 
         return total_loss
 
+    # Define prediction process
     def predict(self, state):
         return self.Actor.predict(state)
--- PPO/Critic.py
+++ PPO/Critic.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense
-from tensorflow.keras.optimizers import Adam, RMSprop
 from tensorflow.keras import backend as K
 
 import numpy as np
@@ -9,28 +8,36 @@ import numpy as np
 class Critic_Model():
     def __init__(self, input_shape, action_space, lr, optimizer):
 
-        X_input = Input(input_shape)
+        # Format input
+        input_Crit = Input(input_shape)
         old_values = Input(shape=(1,))
 
-        V = Dense(512, activation="relu", kernel_initializer='he_uniform')(X_input)
-        V = Dense(256, activation="relu", kernel_initializer='he_uniform')(V)
-        V = Dense(64, activation="relu", kernel_initializer='he_uniform')(V)
-        value = Dense(1, activation=None)(V)
+        # Define Network structure
+        X_Crit = Dense(512, activation="relu", kernel_initializer='he_uniform')(input_Crit)
+        X_Crit = Dense(256, activation="relu", kernel_initializer='he_uniform')(X_Crit)
+        X_Crit = Dense(64, activation="relu", kernel_initializer='he_uniform')(X_Crit)
+        output_Crit = Dense(1, activation=None)(X_Crit)
 
-        self.Critic = Model(inputs=[X_input, old_values], outputs = value)
+        # Create Critic
+        self.Critic = Model(inputs=[input_Crit, old_values], outputs = output_Crit)
         self.Critic.compile(loss=[self.critic_PPO2_loss(old_values)], optimizer=optimizer(lr=lr))
 
+    # Define ProximalPoliyOptimization2 critic loss
     def critic_PPO2_loss(self, values):
         def loss(y_true, y_pred):
+
+            # Calculate clipped loss
             LOSS_CLIPPING = 0.2
             clipped_value_loss = values + K.clip(y_pred - values, -LOSS_CLIPPING, LOSS_CLIPPING)
             v_loss1 = (y_true - clipped_value_loss) ** 2
             v_loss2 = (y_true - y_pred) ** 2
             
-            value_loss = 0.5 * K.mean(K.maximum(v_loss1, v_loss2)) # tweaked PPO loss function
-            #value_loss = K.mean((y_true - y_pred) ** 2) # standard PPO loss
+            # Use tweaked PPO loss function to get final loss
+            value_loss = 0.5 * K.mean(K.maximum(v_loss1, v_loss2))
+
             return value_loss
         return loss
 
+    # Define prediction process
     def predict(self, state):
         return self.Critic.predict([state, np.zeros((state.shape[0], 1))])
